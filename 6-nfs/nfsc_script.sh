43eb8e9bfa86f5539d4639518ea67aa21ccb83d9
newbieSmollett <164991154+newbieSmollett@users.noreply.github.com>
2024-07-14 20:13:26 +0300

    Add files via upload

---
+++ 6-nfs/nfsc_script.sh
@@ -0,0 +1,4 @@
+yum install nfs-utils -y
+echo "192.168.56.10:/srv/share/ /mnt nfs vers=3,proto=udp,noauto,x-systemd.automount 0 0" >>/etc/fstab
+systemctl daemon-reload
+systemctl restart remote-fs.target
